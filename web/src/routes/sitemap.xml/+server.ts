1ef5e980bbd1fa026a1921a1b14adc0305cc6e7b
Pablo Fernandez <pfer@me.com>
2026-04-14 16:34:29 +0000

    feat: add sitemap and robots for SEO (#7)

---
+++ web/src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,61 @@
+import type { RequestHandler } from './$types';
+import { fetchRecentMarkets } from '$lib/server/cascade';
+
+type SitemapPage = {
+  loc: string;
+  priority: string;
+  changefreq: string;
+  lastmod?: string;
+};
+
+function escapeXml(value: string): string {
+  return value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&apos;');
+}
+
+export const GET: RequestHandler = async ({ url }) => {
+  const origin = url.origin;
+
+  const staticPages: SitemapPage[] = [
+    { loc: origin, priority: '1.0', changefreq: 'hourly' },
+    { loc: `${origin}/about`, priority: '0.8', changefreq: 'monthly' },
+    { loc: `${origin}/how-it-works`, priority: '0.8', changefreq: 'monthly' },
+    { loc: `${origin}/leaderboard`, priority: '0.6', changefreq: 'daily' }
+  ];
+
+  const markets = await fetchRecentMarkets(500);
+  const marketPages: SitemapPage[] = markets.map((market) => ({
+    loc: `${origin}/market/${encodeURIComponent(market.slug)}`,
+    priority: '0.9',
+    changefreq: 'hourly',
+    ...(market.createdAt
+      ? { lastmod: new Date(market.createdAt * 1000).toISOString().split('T')[0] }
+      : {})
+  }));
+
+  const allPages = [...staticPages, ...marketPages];
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allPages
+  .map(
+    (page) => `  <url>
+    <loc>${escapeXml(page.loc)}</loc>
+    <changefreq>${page.changefreq}</changefreq>
+    <priority>${page.priority}</priority>${page.lastmod ? `
+    <lastmod>${page.lastmod}</lastmod>` : ''}
+  </url>`
+  )
+  .join('\n')}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      'content-type': 'application/xml',
+      'cache-control': 'public, max-age=3600, s-maxage=86400'
+    }
+  });
+};
